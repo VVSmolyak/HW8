fe56bbd7a39fdb9831500dfad66c21c51e1a6565
Veronika Smoliak <a.smolyak@cardsmobile.ru>
2023-04-16 22:18:41 +0300

    Program added

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿string[] input = new string[] {"Cat", "Food", "Twenty", "red", "rotten", "go", "0"};
+
+print(getStrings(input));
+
+string[] getStrings(string[] input) {
+    string[] result = new string[input.Length];
+    for (int inputIndex = 0, resultIndex = 0; inputIndex<input.Length; inputIndex++) {
+        if(input[inputIndex].Length <= 3) {
+            result[resultIndex] = input[inputIndex];
+            resultIndex++;
+        }
+    }
+    return result;
+}
+
+void print(string[] array) {
+    string result = "";
+    for(int i = 0; i<array.Length; i++) {
+        result += array[i]  + " ";
+    }
+
+    Console.WriteLine("Result: " + result);
+}
